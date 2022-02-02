5f65296aea2da638f5ae2759bcc1689e18eea013
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-02 21:31:28 +0100

    03-Module-03-ApplicationFrontEndREACT-06-Footer

--- Programmes/03-Module-03-ApplicationFrontEndREACT/myzoo/src/containers/Site/Site.js
+++ Programmes/03-Module-03-ApplicationFrontEndREACT/myzoo/src/containers/Site/Site.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import NavBar from '../../components/UI/NavBar/NavBar';
 import {Switch, Route} from 'react-router-dom';
 import Home from './Home/Home';
-import Error from "../../components/Error/Error";
+import Error from '../../components/Error/Error';
+import Footer from '../../components/UI/Footer/Footer';
 
 class Site extends Component {
     render() {
         return (
             <>
-                <NavBar />
-                <Switch>
-                    <Route path="/contact" exact render={() => <h1>Page de contact</h1>} />
-                    <Route path="/" exact render={() => <Home />} />
-                    <Route render={() => <Error type="404">La page n'existe pas</Error>} />
-                </Switch>
+                <div className="site">
+                    <NavBar />
+                    <Switch>
+                        <Route path="/contact" exact render={() => <h1>Page de contact</h1>} />
+                        <Route path="/" exact render={() => <Home />} />
+                        <Route render={() => <Error type="404">La page n'existe pas</Error>} />
+                    </Switch>
+                    <div className="minSite"></div>
+                </div>
+                <Footer />
             </>
         );
     }
