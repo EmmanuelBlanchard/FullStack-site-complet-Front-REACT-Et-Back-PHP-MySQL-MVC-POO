977efaeecbe6ba7a0bdbf3ab6e82e6e44b85d179
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-02 16:18:41 +0100

    03-Module-03-ApplicationFrontEndREACT-02-CreationDeLaNavbarEtAjoutDeLaFavicon

---
+++ Programmes/03-Module-03-ApplicationFrontEndREACT/myzoo/src/components/UI/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from '../../../assets/images/logo.png';
+
+const navbar = (props) => (
+    <>  
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    <img src={logo} alt="logo du site My Zoo" width="50px" className="rounded" />
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Accueil</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </>
+);
+
+export default navbar;
--- Programmes/03-Module-03-ApplicationFrontEndREACT/myzoo/src/containers/Site/Site.js
+++ Programmes/03-Module-03-ApplicationFrontEndREACT/myzoo/src/containers/Site/Site.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NavBar from '../../components/UI/NavBar/NavBar';
 
 class Site extends Component {
     render() {
         return (
             <>
+                <NavBar />
                 Site des animaux
             </>
         );
