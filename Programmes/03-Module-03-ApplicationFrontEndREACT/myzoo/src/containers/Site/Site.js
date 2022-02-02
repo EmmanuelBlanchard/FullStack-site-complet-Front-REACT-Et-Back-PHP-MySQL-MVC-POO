93c1a4d31e734722606558325e3764061b1857da
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2022-02-02 15:36:05 +0100

    03-Module-03-ApplicationFrontEndREACT-01-CreationDuProjet

---
+++ Programmes/03-Module-03-ApplicationFrontEndREACT/myzoo/src/containers/Site/Site.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Site extends Component {
+    render() {
+        return (
+            <>
+                Site des animaux
+            </>
+        );
+    }
+}
+
+export default Site;
